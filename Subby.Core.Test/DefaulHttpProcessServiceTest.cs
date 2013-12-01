401279732085b890a456ce1f7d226a942ff8504d
Lam Chan <lamchakchan@gmail.com>
2013-11-30 18:55:51 -0600

    started work on http resource service unit test

---
+++ Subby.Core.Test/DefaulHttpProcessServiceTest.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Subby.Core.Model;
+using Subby.Core.Parser.Impl;
+using Subby.Core.Service;
+using Subby.Core.Service.Impl;
+
+namespace Subby.Core.Test
+{
+    [TestClass]
+    [DeploymentItem("Files", "Files")]
+    public class DefaulHttpProcessServiceTest
+    {
+        IHttpResourceProcessService service = new DefaultHttpProcessService(new JsonParser(), new XmlParser(), new NewLineParser());
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void ReadTarget_LoadFileHasContent_True()
+        {
+            var content = service.Read(new HttpTargetContext { HttpResource = new HttpResource{Url = "http://www.google.com"}});
+            Assert.IsTrue(!string.IsNullOrEmpty(content));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void ReadTarget_LoadNonExistentFile_Fail()
+        {
+            var content = service.Read(new HttpTargetContext { HttpResource = new HttpResource { Url = "http://www.google.com/abc" } });
+        }
+
+        //[TestMethod]
+        //public void ReadSource_LoadJsonFileHasContent_True()
+        //{
+        //    var content = service.Read(
+        //        new List<FileSourceContext>
+        //        {
+        //            new FileSourceContext
+        //            {
+        //                Type = SourceType.Json, 
+        //                FilePath = Path.Combine(TestContext.TestDeploymentDir, @"Files\variables.json")
+        //            }
+        //        });
+        //    Assert.IsTrue(content.Any());
+        //}
+
+        //[TestMethod]
+        //public void ReadSource_LoadXmlFileHasContent_True()
+        //{
+        //    var content = service.Read(
+        //        new List<FileSourceContext>
+        //        {
+        //            new FileSourceContext
+        //            {
+        //                Type = SourceType.Xml, 
+        //                FilePath = Path.Combine(TestContext.TestDeploymentDir, @"Files\variables.xml")
+        //            }
+        //        });
+        //    Assert.IsTrue(content.Any());
+        //}
+
+        //[TestMethod]
+        //public void ReadSource_LoadNewLineFileHasContent_True()
+        //{
+        //    var content = service.Read(
+        //        new List<FileSourceContext>
+        //        {
+        //            new FileSourceContext
+        //            {
+        //                Type = SourceType.NewLineDelimited, 
+        //                FilePath = Path.Combine(TestContext.TestDeploymentDir, @"Files\variables.txt")
+        //            }
+        //        });
+        //    Assert.IsTrue(content.Any());
+        //}
+
+        //[TestMethod]
+        //public void ReadSource_LoadJsonFileCountCheck_True()
+        //{
+        //    var content = service.Read(
+        //        new List<FileSourceContext>
+        //        {
+        //            new FileSourceContext
+        //            {
+        //                Type = SourceType.Json, 
+        //                FilePath = Path.Combine(TestContext.TestDeploymentDir, @"Files\variables.json")
+        //            }
+        //        });
+        //    Assert.IsTrue(content.Count == 9);
+        //}
+
+        //[TestMethod]
+        //public void ReadSource_LoadXmlFileCountCheck_True()
+        //{
+        //    var content = service.Read(
+        //        new List<FileSourceContext>
+        //        {
+        //            new FileSourceContext
+        //            {
+        //                Type = SourceType.Xml, 
+        //                FilePath = Path.Combine(TestContext.TestDeploymentDir, @"Files\variables.xml")
+        //            }
+        //        });
+        //    Assert.IsTrue(content.Count == 9);
+        //}
+
+        //[TestMethod]
+        //public void ReadSource_LoadNewLineFileCountCheck_True()
+        //{
+        //    var content = service.Read(
+        //        new List<FileSourceContext>
+        //        {
+        //            new FileSourceContext
+        //            {
+        //                Type = SourceType.NewLineDelimited, 
+        //                FilePath = Path.Combine(TestContext.TestDeploymentDir, @"Files\variables.txt")
+        //            }
+        //        });
+        //    Assert.IsTrue(content.Count == 9);
+        //}
+
+        //[TestMethod]
+        //[ExpectedException(typeof(Exception))]
+        //public void ReadSource_LoadFileWithUnavailableType_Fail()
+        //{
+        //    var content = service.Read(
+        //        new List<FileSourceContext>
+        //        {
+        //            new FileSourceContext
+        //            {
+        //                Type = SourceType.Unknown, 
+        //                FilePath = Path.Combine(TestContext.TestDeploymentDir, @"Files\variables.json")
+        //            }
+        //        });
+        //}
+
+        //[TestMethod]
+        //[ExpectedException(typeof(Exception))]
+        //public void ReadSource_LoadNonExistentFile_Fail()
+        //{
+        //    var content = service.Read(
+        //        new List<FileSourceContext>
+        //        {
+        //            new FileSourceContext
+        //            {
+        //                Type = SourceType.Json, 
+        //                FilePath = Path.Combine(TestContext.TestDeploymentDir, @"Files\nofile")
+        //            }
+        //        });
+        //}
+    }
+}
--- Subby.Core/Service/Impl/DefaultFileProcessService.cs
+++ Subby.Core/Service/Impl/DefaultFileProcessService.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
 using Subby.Core.Enum.Impl;
 using Subby.Core.Model;
 using Subby.Core.Parser;
@@ -100,96 +99,6 @@ namespace Subby.Core.Service.Impl
             }
 
             throw new Exception("Target file does not exist");
-        }
-    }
-
-    public class DefaultHttpProcessService : IHttpResourceProcessService
-    {
-        private readonly IJsonVariableParser _jsonParser;
-        private readonly IXmlVariableParser _xmlParser;
-        private readonly INewLineVariableParser _newLineParser;
-
-        public DefaultHttpProcessService(IJsonVariableParser jsonParser, IXmlVariableParser xmlParser, INewLineVariableParser newLineParser)
-        {
-            _jsonParser = jsonParser;
-            _xmlParser = xmlParser;
-            _newLineParser = newLineParser;
-        }
-
-        public IDictionary<string, string> Read(IList<HttpSourceContext> sources)
-        {
-            if (sources == null)
-            {
-                throw new Exception("Context is null");
-            }
-            if (!sources.Any())
-            {
-                throw new Exception("Context is empty");
-            }
-
-            var result = new Dictionary<string, string>();
-
-            foreach (var source in sources)
-            {
-
-                string content;
-
-                try
-                {
-                    using (var webClient = new WebClient())
-                    {
-                        content = source.HttpResource.Action.Value == HttpMethod.Get.Value
-                            ? webClient.DownloadString(source.HttpResource.Url)
-                            : webClient.UploadString(source.HttpResource.Url, string.Empty);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception(string.Format("Error reading source file ({0})", source.HttpResource.Url), ex);
-                }
-
-                if (source.Type.Value == SourceType.Json.Value)
-                {
-                    result = result.Union(_jsonParser.Parse(content)).ToDictionary(p => p.Key, p => p.Value);
-                }
-                else if (source.Type.Value == SourceType.Xml.Value)
-                {
-                    result = result.Union(_xmlParser.Parse(content)).ToDictionary(p => p.Key, p => p.Value);
-                }
-                else if (source.Type.Value == SourceType.NewLineDelimited.Value)
-                {
-                    result = result.Union(_newLineParser.Parse(content)).ToDictionary(p => p.Key, p => p.Value);
-                }
-                else
-                {
-                    throw new Exception(string.Format("No parser implemented for this content type ({0})",
-                        source.Type.DisplayName));
-                }
-            }
-
-            return result;
-        }
-
-        public string Read(HttpTargetContext target)
-        {
-            if (target == null)
-            {
-                throw new Exception("Context is null");
-            }
-
-            try
-            {
-                using (var webClient = new WebClient())
-                {
-                    return target.HttpResource.Action.Value == HttpMethod.Get.Value
-                        ? webClient.DownloadString(target.HttpResource.Url)
-                        : webClient.UploadString(target.HttpResource.Url, string.Empty);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Error reading target file", ex);
-            }
         }
     }
 }
